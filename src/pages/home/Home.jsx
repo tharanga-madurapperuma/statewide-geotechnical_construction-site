cf7b7b1bf210682ee78b3ce93752218b17c185c0
Rusiru Erandaka <erandak-ec20037@stu.kln.ac.lk>
2025-03-04 19:56:33 +0530

    added section to Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-
+import Callbox from "../../components/callbox/Callbox";
 const Home = () => {
     return (
         <div className="bg-amber-400 w-full h-[1000px] justify-center items-center flex">
-            <p style={{ fontSize: "50px" }} className="font-normal">
-                Home Layed Here
-            </p>
+           <Callbox />
         </div>
     );
 };

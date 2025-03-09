8335042096d536e66a62fae3e948f8a4b4dc936c
Rusiru Erandaka <erandak-ec20037@stu.kln.ac.lk>
2025-03-09 17:23:04 +0530

    remove shadow

--- src/components/card-icon-license/CardIconLicense.jsx
+++ src/components/card-icon-license/CardIconLicense.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const LicenseCard = ({image, title, description}) => {
   return (
-    <div className="flex flex-col items-center justify-center w-[368px] h-[416px] bg-blue-50 rounded-2xl shadow-md p-4 lg:p-8">
+    <div className="flex flex-col items-center justify-center w-[368px] h-[416px] bg-blue-50 rounded-2xl p-4 lg:p-8">
       <div className="flex items-center justify-center w-[168px] h-[168px] bg-blue-600 rounded-4xl">
         <img
           src={image}

cedf3b7588dcf0e03605ae8fdaa2ef575c2e2f0b
Rusiru Erandaka <erandak-ec20037@stu.kln.ac.lk>
2025-03-10 11:42:27 +0530

    epdated margin in mobile view

--- src/components/text/PolicyText.jsx
+++ src/components/text/PolicyText.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PolicyText = ({ title, description, bulletText }) => {
     return (
-        <div className="pl-[110px] pr-[110px]">
+        <div className="md:px-[110px] px-[40px]">
             <h2 className="text-4xl font-bold text-gray-900 mb-7 mt-[20px]">{title}</h2>
             <p className="text-gray-700 mt-2">{description}</p>
             <ul className="list-disc list-inside mt-4 text-gray-700">

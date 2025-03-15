8a319de6103522a9f45384285113fc855c63bfa5
Hashan Tharanga Gamage <109969514+HashanTG@users.noreply.github.com>
2025-03-15 23:15:25 +0530

    i added licencecard section to site page

--- src/pages/Site.jsx
+++ src/pages/Site.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ButtonCard from "../components/card/ButtonCard";
 import CardIconTextCenter from "../components/card-icon-text-center/CardIconTextCenter";
 import InfoSection from "../components/info-section/InfoSection";
+import LicenseCard from "../components/card-icon-license/CardIconLicense";
 
 import {
   InvestigationHero,
@@ -19,8 +20,11 @@ import {
   UndisturbedSampling,
   GroundwaterMonitoringBores,
   PermeabilityTesting,
-    PressureMeterTesting,
-    EnvSecond
+  PressureMeterTesting,
+  EnvSecond,
+  Certificate,
+  Card,
+  Box,
 } from "../assets/assets";
 
 const Site = () => {
@@ -110,39 +114,54 @@ const Site = () => {
             bodyText=""
           />
         </div>
-          </div>
-          
-             {/* Left pic paragraph component */}
-             <div className=" mt-8 sm:mt-40 md:mt-[100px] lg:mt-[128px] mb-8 sm:mb-40 md:mb-[100px] lg:mb-[64px] px-[32px] sm:px-[70px] md:px-80 lg:px-[100px] xl:px-[110px]">
-                <InfoSection
-                    title="Expert Drilling Equipment for Challenging Sites"
-                    description={
-                        <>
-                            We operate{" "}
-                            <span className="text-gray-700 font-500">
-                                Geoprobe, Comacchio,
-                            </span>{" "}
-                            and{" "}
-                            <span className="text-gray-700 font-500">
-                                Custom rigs,
-                            </span>{" "}
-                            designed for small footprint and low headroom
-                            applications—ideal for tight access and
-                            di(icult-to-reach) areas. Our drilling rigs are
-                            equipped with{" "}
-                            <span className="text-gray-700 font-500">
-                                industry-standard safety cages and emergency
-                                shut-off systems
-                            </span>{" "}
-                            to ensure maximum safety at all times.
-                        </>
-                    }
-                    buttonText=""
-                    buttonLink=""
-                    imageSrc={EnvSecond}
-                    reverse={false}
-                />
-            </div>
+      </div>
+
+      {/* Left pic paragraph component */}
+      <div className=" mt-8 sm:mt-40 md:mt-[100px] lg:mt-[128px] mb-8 sm:mb-40 md:mb-[100px] lg:mb-[64px] px-[32px] sm:px-[70px] md:px-80 lg:px-[100px] xl:px-[110px]">
+        <InfoSection
+          title="Expert Drilling Equipment for Challenging Sites"
+          description={
+            <>
+              We operate{" "}
+              <span className="text-gray-700 font-500">
+                Geoprobe, Comacchio,
+              </span>{" "}
+              and <span className="text-gray-700 font-500">Custom rigs,</span>{" "}
+              designed for small footprint and low headroom applications—ideal
+              for tight access and di(icult-to-reach) areas. Our drilling rigs
+              are equipped with{" "}
+              <span className="text-gray-700 font-500">
+                industry-standard safety cages and emergency shut-off systems
+              </span>{" "}
+              to ensure maximum safety at all times.
+            </>
+          }
+          buttonText=""
+          buttonLink=""
+          imageSrc={EnvSecond}
+          reverse={false}
+        />
+      </div>
+
+      <div className=" bg-white w-full max-w-[1920px] h-auto py-[64px] px-[32px] sm:px-[70px] md:px-80 lg:px-[100px] xl:px-[110px]">
+        <div className="flex flex-col items-start w-full">
+          <h1 className="w-[80%] mb-4 font-inter text-28 sm:text-40 md:text-40 lg:text-48 font-600">
+            Qualified & Experienced Drillers
+          </h1>
+          <p className="font-inter font-400 text-14 mt-2 mb-4 md:text-18 lg:text-24">
+            Our team of highly qualified and experienced drillers hold
+            certifications and competencies in key areas, including:
+          </p>
+        </div>
+        <div className="w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 flex-col sm:flex-row justify-center">
+          <LicenseCard
+            image={Certificate}
+            title="Water Bore Licenses (Class 1 & 2)"
+          />
+          <LicenseCard image={Card} title="Building Industry White Cards" />
+          <LicenseCard image={Box} title="First Aid Certification" />
+        </div>
+      </div>
     </div>
   );
 };
